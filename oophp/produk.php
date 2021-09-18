5b73e91b644ca3120361514cfb6103ccccec4845
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-09-18 12:51:59 +0700

    video 3
    ditimpa semuanya

--- oophp/produk.php
+++ oophp/produk.php
@@ -13,11 +13,18 @@ class Produk {
 
 }
 
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk2 = new Produk();
-$produk2->judul = "kabaneri";
-$produk2->tambahProperty = "baru";
-var_dump($produk2);
+// $produk2 = new Produk();
+// $produk2->judul = "kabaneri";
+// $produk2->tambahProperty = "baru";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis ="Makasi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 400000;
+var_dump($produk3);
