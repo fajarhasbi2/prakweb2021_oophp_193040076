799046b0311f09ff29d01312bbe03e7a8da9f59a
fajarhasbi2 <58681160+fajarhasbi2@users.noreply.github.com>
2021-09-18 07:46:59 +0700

    video 3
    menyelesaikan video 3 latihan memperhatikan kalau mau menimpa harus sama dengan propertinya jika berbeda maka akan menjadi property baru

--- oophp/produk.php
+++ oophp/produk.php
@@ -5,13 +5,19 @@
 // Game
 
 class Produk {
-    public $judul,
-            $penulis,
-            $penerbit,
-            $harga;
+    public $judul = "judul",
+            $penulis = "penulis",
+            $penerbit = "penerbit",
+            $harga = 0;
 
 
 }
 
 $produk1 = new Produk();
+$produk1->judul = "Naruto";
 var_dump($produk1);
+
+$produk2 = new Produk();
+$produk2->judul = "kabaneri";
+$produk2->tambahProperty = "baru";
+var_dump($produk2);
